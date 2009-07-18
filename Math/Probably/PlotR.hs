7239b19b78c6f420be65af9bc954a7d44a936c1e
Tom Nielsen <tomn@tomn-desktop.(none)>
2009-07-18 22:04:14 +0100

    simple plots with r

--- Math/Probably/PlotR.hs
+++ Math/Probably/PlotR.hs
@@ -1,32 +1,52 @@
-module PlotR where
+module Math.Probably.PlotR where
 
 import System.Cmd
 import System.Directory
+import Data.List
+import System.Random
 
 data RPlotCmd = RPlCmd {
-      plotData :: String,
+--      plotData :: String,
       prePlot :: [String],
-      plotArg :: String
+      plotArgs :: [String]
 }
+
+idInt :: Int -> Int
+idInt = id
 
 class PlotWithR a where
     getRPlotCmd :: a -> IO RPlotCmd
    
 plotWithR :: PlotWithR a => a -> IO ()
-
-plotWithR :: V -> IO ()
-plotWithR (SigV t1 t2 dt sf) = do
-  (r::Int) <- randomRIO (0,10000)
-  let datfile= "/tmp/bugplot"++show r
-  let rfile = "/tmp/bugplot"++show r++".r"
-  writeFile datfile $ unlines $ map (\t->show . unsafeVToDbl $ sf t) [0..round $ (t2-t1)/dt]
-  writeFile rfile $ concat [
-                 "x11(width=10,height=7)\n",
-                 "dat <- ts(scan(\"", datfile, "\"), start=", show t1, ", frequency=", show (1/dt),")\n", 
-                 "plot(dat)\n", 
-                 "z<-locator(1)\n",
+plotWithR pl' = do
+  pl <-  getRPlotCmd pl' 
+  r <- (show . idInt) `fmap` randomRIO (0,10000)
+  let rfile = "/tmp/bugplot"++r++".r"
+  writeFile rfile $ unlines [
+                 "x11(width=10,height=7)",
+                 unlines $ prePlot pl,
+                 "plot("++(head $ plotArgs pl)++")", 
+                 "z<-locator(1)",
                  "q()"]
   system $ "R --vanilla --slave < "++rfile
-  removeFile datfile
   removeFile rfile
   return ()
+
+newtype Points a = Points [a]
+
+instance Num a => PlotWithR (Points a) where
+    getRPlotCmd (Points xs) = 
+        return $ RPlCmd {
+                     prePlot = ["xs <- c("++(intercalate ", " $ map show xs)++")"],
+                     plotArgs = ["xs"] }
+instance (PlotWithR a, PlotWithR b) => PlotWithR (a,b) where
+    getRPlotCmd (xs,ys) = do
+      px <- getRPlotCmd xs
+      py <- getRPlotCmd ys                          
+      return $ RPlCmd {
+                     prePlot = prePlot py++prePlot px,
+                     plotArgs =  plotArgs px++ plotArgs py }
+                               
+
+--plotWithR :: V -> IO ()
+--plotWithR (SigV t1 t2 dt sf) = do
