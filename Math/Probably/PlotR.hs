419e7e2930686ecced5ebd2aaf44c8b722107bf1
Tom Nielsen <tomn@tomn-desktop.(none)>
2009-07-20 17:52:29 +0100

    more work on plot r

--- Math/Probably/PlotR.hs
+++ Math/Probably/PlotR.hs
@@ -3,13 +3,31 @@ module Math.Probably.PlotR where
 import System.Cmd
 import System.Directory
 import Data.List
-import System.Random
+import Data.Unique
 
 data RPlotCmd = RPlCmd {
 --      plotData :: String,
       prePlot :: [String],
-      plotArgs :: [String]
-} deriving Show
+      plotArgs :: [PlotLine],
+      cleanUp :: IO ()
+}
+
+data PlotLine = TimeSeries String | Histo String | PLPoints [(Double,Double)] | PLLines [(Double,Double)]
+
+plotLines :: [PlotLine] -> String
+plotLines pls = let tss = [ts | TimeSeries ts <- pls ]
+                    pts = [ts | PLPoints ts <- pls ]
+                    lns = [ts | PLLines ts <- pls ] in
+                case tss of 
+                  [] -> plotLinesNoTS lns pts
+                  tsss -> unlines ["ts.plot("++(intercalate "," tsss)++")",
+                                   unlines $ map lnsplot lns,
+                                   unlines $ map ptplot pts]
+    where lnsplot ln = ""
+          ptplot pt = let (xs,ys) = unzip pt in 
+                      "points(c("++(intercalate "," $ map show xs)++"), c("++(intercalate "," $ map show xs)++"))"
+
+plotLinesNoTS lns pts = "not implemented"--figure out dimensions
 
 idInt :: Int -> Int
 idInt = id
@@ -20,36 +38,38 @@ class PlotWithR a where
 plotWithR :: PlotWithR a => a -> IO ()
 plotWithR pl' = do
   pl <-  getRPlotCmd pl' 
-  r <- (show . idInt) `fmap` randomRIO (0,10000)
-  print pl
+  r <- (show. hashUnique) `fmap` newUnique
+  --print pl
   let rfile = "/tmp/bugplot"++r++".r"
   let rlines = unlines [
                  "x11(width=10,height=7)",
                  unlines $ prePlot pl,
-                 unlines $ map (\plArg-> "plot("++plArg++")") $ plotArgs pl,
+                 plotLines $ plotArgs pl,
                  "z<-locator(1)",
                  "q()"]
   writeFile rfile $ rlines
   putStrLn rlines
   system $ "R --vanilla --slave < "++rfile
   removeFile rfile
+  cleanUp pl
   return ()
 
 newtype Points a = Points [a]
 
-instance Num a => PlotWithR (Points a) where
+instance Real a => PlotWithR (Points a) where
     getRPlotCmd (Points xs) = do 
-        r <- (show . idInt) `fmap` randomRIO (0,10000)
+        r <- hashUnique `fmap` newUnique
         return $ RPlCmd {
-                     prePlot = ["xs"++r++" <- c("++(intercalate ", " $ map show xs)++")"],
-                     plotArgs = ["xs"++r] }
+                     prePlot = [],
+                     plotArgs = [PLPoints $ zip [0..] $ map realToFrac xs], cleanUp = return () }
 instance (PlotWithR a, PlotWithR b) => PlotWithR (a,b) where
     getRPlotCmd (xs,ys) = do
       px <- getRPlotCmd xs
       py <- getRPlotCmd ys                          
       return $ RPlCmd {
                      prePlot = prePlot py++prePlot px,
-                     plotArgs = plotArgs px++(map (++", add=TRUE") $ plotArgs py) }
+                     plotArgs = plotArgs px++plotArgs py, 
+                     cleanUp = cleanUp px >> cleanUp px }
                                
 test = plotWithR (Points [1,2,3], Points [4,5,6])
 
