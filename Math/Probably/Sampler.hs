40b41d739547e4b9715ceb00ab8384440cfa28aa
Tom Nielsen <tomn@tomn-desktop.(none)>
2010-01-19 12:58:10 +0000

    specialised functions for gaussian samplers

--- Math/Probably/Sampler.hs
+++ Math/Probably/Sampler.hs
@@ -71,9 +71,20 @@ gauss m sd =
     do (u1,u2) <- (mapPair realToFrac) `fmap` joint unitSample unitSample
        return $ sqrt(-2*log(u1))*cos(2*pi*u2)*sd+m
 
+gaussD :: Double -> Double -> Sampler Double
+gaussD m sd = 
+    do (u1,u2) <- joint unitSample unitSample
+       return $ sqrt(-2*log(u1))*cos(2*pi*u2)*sd+m
+
+
 gaussMany :: Floating b => [(b,b)] -> Sampler [b]
 gaussMany means_sds = do gus <- gaussManyUnit (length means_sds)
                          return $ map f $ zip gus means_sds
+    where f (gu, (mean, sd)) = gu*sd+mean
+
+gaussManyD :: [(Double,Double)] -> Sampler [Double]
+gaussManyD means_sds = do gus <- gaussManyUnitD (length means_sds)
+                          return $ map f $ zip gus means_sds
     where f (gu, (mean, sd)) = gu*sd+mean
 
 gaussManyUnit :: Floating b => Int -> Sampler [b]
@@ -81,11 +92,21 @@ gaussManyUnit 0 = return []
 gaussManyUnit n | odd n = liftM2 (:) (gauss 0 1) (gaussManyUnit (n-1))
                 | otherwise = do us <- forM [1..n] $ const $ unitSample
                                  return $ gaussTwoAtATime $ map realToFrac us
+
+gaussManyUnitD :: Int -> Sampler [Double]
+gaussManyUnitD 0 = return []
+gaussManyUnitD n | odd n = liftM2 (:) (gauss 0 1) (gaussManyUnit (n-1))
+                | otherwise = do us <- forM [1..n] $ const $ unitSample
+                                 return $ gaussTwoAtATimeD us
                                  
 
 gaussTwoAtATime :: Floating a =>  [a] -> [a]
 gaussTwoAtATime (u1:u2:rest) = sqrt(-2*log(u1))*cos(2*pi*u2) : sqrt(-2*log(u1))*sin(2*pi*u2) : gaussTwoAtATime rest
 gaussTwoAtATime _ = []
+
+gaussTwoAtATimeD :: [Double] -> [Double]
+gaussTwoAtATimeD (u1:u2:rest) = sqrt(-2*log(u1))*cos(2*pi*u2) : sqrt(-2*log(u1))*sin(2*pi*u2) : gaussTwoAtATime rest
+gaussTwoAtATimeD _ = []
 
 bernoulli :: Double -> Sampler Bool
 bernoulli p = (<p) `fmap` unitSample 
