06d0a1ce6a04ca00f336a49f12fa86c47faa7d64
Tom Nielsen <tomn@tomn-desktop.(none)>
2012-11-28 15:58:18 +0000

    Mala: freeze after burning. More feedback. Initial run predicated on accepted samples

--- Math/Probably/MALA.hs
+++ Math/Probably/MALA.hs
@@ -25,12 +25,14 @@ data MalaPar = MalaPar { mpXi :: Vector Double,
                          mpGradLast :: Maybe (Vector Double),
                          mpSigma :: Double,
                          mpCount :: Double,
-                         mpAccept :: Double }
+                         mpAccept :: Double,
+                         mpFreezeSigma :: Bool }
+  deriving Show
 
 mala1 :: Matrix Double -> (forall a. (Real a, Floating a) => [a]->a) 
          -> MalaPar
          -> Sampler MalaPar
-mala1 cov pdf (MalaPar xi pi mbgrad sigma tr tracc) = do
+mala1 cov pdf (MalaPar xi pi mbgrad sigma tr tracc freeze) = do
   let pdfv :: Vector Double -> Double
       pdfv v = pdf $ toList $ v
   let gradienti = case mbgrad of 
@@ -48,12 +50,12 @@ mala1 cov pdf (MalaPar xi pi mbgrad sigma tr tracc) = do
   let tr' = max 1 tr
   if u < exp ratio
      then return $ MalaPar xstar pstar (Just gradientStar) 
-                           ((min 1.4 $ 1+kmala/tr')*sigma) 
-                           (tr+1) (tracc+1)
+                           (if freeze then sigma else (min 1.4 $ 1+kmala/tr')*sigma) 
+                           (tr+1) (tracc+1) freeze
 --                           sigma (tr+1) (tracc+1)
      else return $ MalaPar xi pi (Just gradienti) 
-                           ((max 0.7143 $ 1-kmala/tr')**1.3*sigma) 
-                           (tr+1) tracc
+                           (if freeze then sigma else (max 0.7143 $ 1-kmala/tr')**1.3*sigma) 
+                           (tr+1) tracc freeze
 --                           sigma (tr+1) tracc
 
   
@@ -66,7 +68,7 @@ runMala cov pdf nsam init = go nsam mp1 [] where
   go n y xs = do y1 <- sample $ mala1 cov pdf y
                  go (n-1) y1 $ (mpPi y1, mpXi y1):xs 
   pi = pdf $ toList $ init
-  mp1 =  MalaPar init pi Nothing 1 0 0
+  mp1 =  MalaPar init pi Nothing 1 0 0 False
 
 runMalaMP :: Matrix Double -> (forall a. (Real a, Floating a) => [a]->a) 
          ->  Int -> MalaPar -> [(Double,Vector Double)] -> RIO (MalaPar, [(Double,Vector Double)])
@@ -78,6 +80,19 @@ runMalaMP cov pdf nsam init xs0 = go nsam init xs0 where
                  go (n-1) y1 $ (mpPi y1, mpXi y1):xs 
   pi = pdf $ toList $ mpXi init
 
+runMalaMPaccept :: Matrix Double -> (forall a. (Real a, Floating a) => [a]->a) 
+         ->  Int -> MalaPar -> [(Double,Vector Double)] -> RIO (MalaPar, [(Double,Vector Double)])
+runMalaMPaccept  cov pdf nsam init xs0 = go init xs0 where
+  go mpar xs 
+    | (round $ mpAccept mpar) >= nsam = do 
+         io $ putStrLn $ "MALA accept = "++show (mpAccept mpar/mpCount mpar)
+         io $ putStrLn $ "MALA sigma = "++show (mpSigma mpar)
+         return (mpar, xs)
+    | otherwise = do 
+         mpar1 <- sample $ mala1 cov pdf mpar
+         go mpar1 $ (mpPi mpar1, mpXi mpar1):xs 
+  pi = pdf $ toList $ mpXi init
+
   
  
 kmala = 10
@@ -87,7 +102,7 @@ runMalaRioESS ::  Matrix Double -> (forall a. (Real a, Floating a) => [a]->a)
 runMalaRioESS cov pdf want_ess xi = do
     let p0 = pdf $ toList $ xi
     let sigma0 = 1.5 / (realToFrac $ dim xi) -- determined empirically
-    let mp0 = MalaPar xi p0 Nothing sigma0 0 0 
+    let mp0 = MalaPar xi p0 Nothing sigma0 0 0 False
         nsam0 = want_ess*20
     io $ putStrLn $ "initial sigma = "++show (mpSigma mp0)
     (mp1, xs1) <- runMalaMP cov pdf nsam0 mp0 []
@@ -105,10 +120,11 @@ runMalaRioCodaESS ::  Matrix Double -> (forall a. (Real a, Floating a) => [a]->a
 runMalaRioCodaESS cov pdf want_ess xi = do
     let p0 = pdf $ toList $ xi
     let sigma0 = 1.5 / (realToFrac $ dim xi) -- determined empirically
-    let mp0 = MalaPar xi p0 Nothing sigma0 0 0 
-        nsam0 = want_ess*5
+    let mp0 = MalaPar xi p0 Nothing sigma0 0 0 False
+        nsam0 = want_ess*1
     io $ putStrLn $ "initial sigma = "++show (mpSigma mp0)
-    (mp1, xs1) <- runMalaMP cov pdf nsam0 mp0 []
+    (mp1, xs1) <- runMalaMPaccept cov pdf nsam0 mp0 []
+    io $ print mp1
     let converged mp xs = do
          let have_ess = min (mpAccept mp1) $ calcESSprim $ map snd xs1
          if have_ess > realToFrac want_ess
@@ -116,16 +132,20 @@ runMalaRioCodaESS cov pdf want_ess xi = do
             else do let need_ess =  max 1 $ realToFrac want_ess - have_ess
                         samples_per_es = realToFrac nsam0/have_ess
                         to_do = round $ samples_per_es * need_ess 
-                    (mp2, xs2) <- runMalaMP cov pdf to_do mp1 xs1
+                    io $ putStrLn $ "running converged for "++show to_do
+                    (mp2, xs2) <- runMalaMP cov pdf to_do (mp1 {mpFreezeSigma = True}) xs1
                     return $ map snd xs2 
     let go mp n xs = do
             (mp2, xs2) <- runMalaMP cov pdf n mp []
-            if  mannWhitneyUtest TwoTailed 0.05 (U.fromList $ map fst xs2)
+            if  mannWhitneyUtest TwoTailed 0.10 (U.fromList $ map fst xs2)
                                             (U.fromList $ map fst xs) /= Just NotSignificant
-               then go mp2 (round $ realToFrac n*1.5) xs2
-               else converged mp2 (xs2++xs)
+               then do io$ putStrLn "not converged"
+                       go mp2 (round $ realToFrac n*1.5) xs2
+               else do io$ putStrLn "converged!"
+                       io $ print mp2
+                       converged mp2 (xs2++xs)
 
-    go mp1 nsam0 xs1
+    go mp1 (round $ mpCount mp1) xs1
 
 
 
