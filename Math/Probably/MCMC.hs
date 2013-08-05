8a5942059264c3a4430d90007ca08076b12629bb
Tom Nielsen <tomn@openbrain.org>
2013-08-05 23:07:37 +0100

    begin runChain. Interim checkin, wont compile

--- Math/Probably/MCMC.hs
+++ Math/Probably/MCMC.hs
@@ -11,6 +11,9 @@ type PosteriorDensity = Double
 --function to calculate posterior and gradient 
 type PostGradF =  Parameters -> (PosteriorDensity, Gradient)
 
+--function to calculate posterior  
+type PosteriorF =  Parameters -> PosteriorDensity
+
 
 -- type parameter `extra` is the extra information an algorithm gets to lug around.
 --   use for e.g. adaptation parameters.
@@ -18,19 +21,34 @@ type PostGradF =  Parameters -> (PosteriorDensity, Gradient)
 data Strategy extra
       -- GStrategy: based on gradients. the first 
     = GStrategy ( PostGradF
-                 -> Parameters --current point in parameter space
-                 -> extra -- current "stuff"
-                 -> (PosteriorDensity, Gradient) --previous gradient and posterior 
-                 -> Prob ((Parameters, extra), --next point in param space
-                          (Double, Vector Double))) -- return postgrad for caching
-                (Vector Double -> extra) --initialise extra from initial val
+                   -> Parameters --current point in parameter space
+                   -> extra -- current "stuff"
+                   -> (PosteriorDensity, Gradient) --previous gradient and posterior 
+                   -> Prob ((Parameters, extra), --next point in param space
+                            (PosteriorDensity, Gradient))) -- return postgrad for caching
+                (Parameters -> extra) --initialise extra from initial val
       --non-gradient based
-    | VStrategy ((Parameters -> PosteriorDensity) 
+    | VStrategy (PosteriorF
                  -> Parameters --current point
                  -> extra 
-                 -> Double --prev posterior val for caching
-                 -> Prob ((Vector Double, extra), Double)) 
-                (Vector Double -> extra) --"extra" initialiser
+                 -> PosteriorDensity --prev posterior val for caching
+                 -> Prob ((Parameters, extra), PosteriorDensity)) 
+                (Parameters -> extra) --"extra" initialiser
+
+
+runChain :: Prob Parameters -> PosteriorF -> PostGradF -> Int -> Int -> Strategy a -> Prob [Parameters]
+runChain inisam posterior postgrad thinn nsamples strat = do
+   ini <- inisam
+   let pg = postgrad ini
+   stuff0 = case strat of
+              GStrategy _ inistuff -> inistuff ini
+              VStrategy _ inistuff -> inistuff ini
+   runChain' ini 
+ 
+runChain' :: Parameters -> PosteriorF -> PostGradF -> Int -> Int -> a -> (PosteriorDensity, Gradient) -> Strategy a -> Prob [Parameters]
+runChain' x0 posterior postgrad _ 0 _ _ = return []
+runChain' x0 posterior postgrad thin iters stuff0 (VStrategy f _) = do
+      ((x1,stuff1), p1)
 
 
 rwm = VStrategy rwmTrans rwmIni
@@ -51,3 +69,4 @@ rwmTrans posterior xi (sigma,(i, iaccept)) pi = do
    
 
 rwmIni _ = (0.1,(1.0, 0.0))
+
