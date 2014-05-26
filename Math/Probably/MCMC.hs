cfc0aa33a0fa694dd2a499064ff5bda6c32b9bb6
Jared Tobin <jared@jtobin.ca>
2014-05-26 13:55:26 +1000

    Add sampling-based interleave operators.

--- Math/Probably/MCMC.hs
+++ Math/Probably/MCMC.hs
@@ -1,6 +1,7 @@
 {-# LANGUAGE ScopedTypeVariables #-}
 
 module Math.Probably.MCMC (
+  -- * Strategies
     continuousSlice
   , discreteSlice
   , hamiltonian
@@ -8,18 +9,22 @@ module Math.Probably.MCMC (
   , metropolis
   , nuts
   , nutsDualAveraging
+  -- * Interleaving
+  , interleave
+  , polyInterleave
+  , frequency
+  , firstWithProb
+  , oneOfRandomly
+  -- * Other
+  , ezMC
   , trace
   ) where
 
 import Control.Applicative
 import Control.Monad
 import Control.Monad.State.Strict
-import Data.Map (Map)
-import qualified Data.Map as Map
-import Data.Maybe
 import Math.Probably.Sampler
 import Math.Probably.Types
-import Numeric.LinearAlgebra
 import Strategy.Hamiltonian
 import Strategy.MALA
 import Strategy.Metropolis
@@ -27,25 +32,25 @@ import Strategy.NUTS
 import Strategy.NUTSDualAveraging
 import Strategy.Slice
 
--- interleave :: [Transition a] -> Transition a
--- interleave = foldl1 (>>) 
--- 
--- oneOfRandomly :: [Transition a] -> Transition a
--- oneOfRandomly ts = do
---   j <- lift $ oneOf [0..(length ts - 1)]
---   ts !! j
--- 
--- frequency :: [(Int, Transition a)] -> Transition a
--- frequency xs = lift (oneOf [1..tot]) >>= (`pick` xs) where
---   tot = sum . map fst $ xs
---   pick n ((k, v):vs)
---     | n <= k    = v
---     | otherwise = pick (n - k) vs
--- 
--- firstWithProb :: Double -> Transition a -> Transition a -> Transition a
--- firstWithProb p t0 t1 = do
---   s <- lift $ bernoulli p
---   if s then t0 else t1
+interleave :: [Transition a] -> Transition a
+interleave = foldl1 (>>) 
+
+oneOfRandomly :: [Transition a] -> Transition a
+oneOfRandomly ts = do
+  j <- lift $ oneOf [0..(length ts - 1)]
+  ts !! j
+
+frequency :: [(Int, Transition a)] -> Transition a
+frequency xs = lift (oneOf [1..tot]) >>= (`pick` xs) where
+  tot = sum . map fst $ xs
+  pick n ((k, v):vs)
+    | n <= k    = v
+    | otherwise = pick (n - k) vs
+
+firstWithProb :: Double -> Transition a -> Transition a -> Transition a
+firstWithProb p t0 t1 = do
+  s <- lift $ bernoulli p
+  if s then t0 else t1
 
 trace :: Int -> Transition t -> Chain t -> Prob (Prob Parameters)
 trace n t o = Samples <$> replicateM n t `evalStateT` o
