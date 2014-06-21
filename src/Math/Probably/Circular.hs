0843da114374576237b77109dc5c8ec1e5de886e
Tom Nielsen <tomn@brix>
2014-06-21 22:36:53 +0100

    hmatrix 0.16; cicular dispersion

--- src/Math/Probably/Circular.hs
+++ src/Math/Probably/Circular.hs
@@ -5,9 +5,15 @@ import Numeric.LinearAlgebra
 import Control.Applicative
 
 --approximate method first mentioned http://en.wikipedia.org/wiki/Mean_of_circular_quantities
-approxCircularMean :: [Double] -> Double
-approxCircularMean = runStat (after vmean getAngle) . map toCartesian where
+circularMean :: [Double] -> Double
+circularMean = runStat (after vmean getAngle) . map toCartesian where
   vmean = pure (scale) <*> after realLengthF recip <*> sumF
   getAngle v = atan2 (v@>1) (v@>0)
   toCartesian alpha = fromList [cos alpha, sin alpha]
 
+--http://webspace.ship.edu/pgmarr/Geo441/Lectures/Lec%2016%20-%20Directional%20Statistics.pdf
+circularDispersion :: [Double] -> Double
+circularDispersion = runStat (after (both xs ys) getR) where
+  xs = before sumF cos
+  ys = before sumF sin
+  getR (x,y) = sqrt (x*x + y*y)
