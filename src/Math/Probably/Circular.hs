204e8b97d21f381371cefc45eef6a72b3f78d85d
Tom Nielsen <tanielsen@gmail.com>
2014-06-23 09:35:00 +0100

    circular stats are now folds, with example for running

--- src/Math/Probably/Circular.hs
+++ src/Math/Probably/Circular.hs
@@ -5,15 +5,21 @@ import Numeric.LinearAlgebra
 import Control.Applicative
 
 --approximate method first mentioned http://en.wikipedia.org/wiki/Mean_of_circular_quantities
-circularMean :: [Double] -> Double
-circularMean = runStat (after vmean getAngle) . map toCartesian where
+circularMean :: Fold Double Double
+circularMean = before (after vmean getAngle) toCartesian where
   vmean = pure (scale) <*> after realLengthF recip <*> sumF
   getAngle v = atan2 (v@>1) (v@>0)
   toCartesian alpha = fromList [cos alpha, sin alpha]
 
 --http://webspace.ship.edu/pgmarr/Geo441/Lectures/Lec%2016%20-%20Directional%20Statistics.pdf
-circularDispersion :: [Double] -> Double
-circularDispersion = runStat (after (both xs ys) getR) where
+circularDispersion :: Fold Double Double
+circularDispersion = after (both xs ys) getR where
   xs = before sumF cos
   ys = before sumF sin
   getR (x,y) = sqrt (x*x + y*y)
+
+
+--example for running statistic
+
+runCircularMean :: [Double] -> Double
+runCircularMean = runStat circularMean
